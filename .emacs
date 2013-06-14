10ca4f542af4d0874ca44464a6bc129018baef93
bryisazombie <bryanzettler@gmail.com>
2013-06-14 11:22:52 -0500

    pushing shit

--- .emacs
+++ .emacs
@@ -6,9 +6,8 @@
 
 ; Multiple cursors
 (require 'multiple-cursors)
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C->") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+(global-set-key "\C-d" 'mc/mark-next-like-this)
+(global-set-key "\M-d" 'mc/mark-all-like-this)
 
 ; Ido mode
 (require 'ido)
@@ -67,6 +66,7 @@
 ; Helpful key bindings
 (global-set-key "\C-^" 'enlarge-window)
 (global-set-key "\M-g" 'goto-line)
+(global-set-key "\M-;" 'evilnc-comment-or-uncomment-lines)
 
 ; Tab 2
 (setq js-indent-level 2)
@@ -81,7 +81,7 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector [default bold shadow italic underline bold bold-italic bold])
  '(ansi-color-names-vector (vector "#cccccc" "#f2777a" "#99cc99" "#ffcc66" "#6699cc" "#cc99cc" "#66cccc" "#2d2d2d"))
- '(custom-enabled-themes (quote (base16-eighties)))
+ '(custom-enabled-themes (quote (base16-default)))
  '(custom-safe-themes (quote ("9bac44c2b4dfbb723906b8c491ec06801feb57aa60448d047dbfdbd1a8650897" "1affe85e8ae2667fb571fc8331e1e12840746dae5c46112d5abb0c3a973f5f5a" "ae8d0f1f36460f3705b583970188e4fbb145805b7accce0adb41031d99bd2580" "405fda54905200f202dd2e6ccbf94c1b7cc1312671894bc8eca7e6ec9e8a41a2" "41b6698b5f9ab241ad6c30aea8c9f53d539e23ad4e3963abff4b57c0f8bf6730" "978ff9496928cc94639cb1084004bf64235c5c7fb0cfbcc38a3871eb95fa88f6" "f41fd682a3cd1e16796068a2ca96e82cfd274e58b978156da0acce4d56f2b0d5" "de2c46ed1752b0d0423cde9b6401062b67a6a1300c068d5d7f67725adc6c3afb" "e53cc4144192bb4e4ed10a3fa3e7442cae4c3d231df8822f6c02f1220a0d259a" "51bea7765ddaee2aac2983fac8099ec7d62dff47b708aa3595ad29899e9e9e44" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "3d6b08cd1b1def3cc0bc6a3909f67475e5612dba9fa98f8b842433d827af5d30" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" default)))
  '(fci-rule-color "#393939")
  '(vc-annotate-background nil)
--- .emacs.d/elpa/multiple-cursors-20130608.1031/multiple-cursors-autoloads.el
+++ .emacs.d/elpa/multiple-cursors-20130608.1031/multiple-cursors-autoloads.el
@@ -4,8 +4,8 @@
 
 
 ;;;### (autoloads (mc/edit-beginnings-of-lines mc/edit-ends-of-lines
-;;;;;;  mc/edit-lines) "mc-edit-lines" "mc-edit-lines.el" (20921
-;;;;;;  62328 892447 192000))
+;;;;;;  mc/edit-lines) "mc-edit-lines" "mc-edit-lines.el" (20923
+;;;;;;  15466 653747 697000))
 ;;; Generated autoloads from mc-edit-lines.el
 
 (autoload 'mc/edit-lines "mc-edit-lines" "\
@@ -36,7 +36,7 @@ Add one cursor to the beginning of each line in the active region.
 ;;;;;;  mc/mark-next-lines mc/mark-previous-symbol-like-this mc/mark-previous-word-like-this
 ;;;;;;  mc/mark-previous-like-this mc/mark-next-symbol-like-this
 ;;;;;;  mc/mark-next-word-like-this mc/mark-next-like-this) "mc-mark-more"
-;;;;;;  "mc-mark-more.el" (20921 62328 864447 192000))
+;;;;;;  "mc-mark-more.el" (20923 15466 525747 693000))
 ;;; Generated autoloads from mc-mark-more.el
 
 (autoload 'mc/mark-next-like-this "mc-mark-more" "\
@@ -179,8 +179,8 @@ Mark the tag we're in and its pair for renaming.
 
 ;;;***
 
-;;;### (autoloads (mc/mark-pop) "mc-mark-pop" "mc-mark-pop.el" (20921
-;;;;;;  62328 908447 194000))
+;;;### (autoloads (mc/mark-pop) "mc-mark-pop" "mc-mark-pop.el" (20923
+;;;;;;  15466 717747 700000))
 ;;; Generated autoloads from mc-mark-pop.el
 
 (autoload 'mc/mark-pop "mc-mark-pop" "\
@@ -193,8 +193,8 @@ line point is on.
 ;;;***
 
 ;;;### (autoloads (mc/sort-regions mc/reverse-regions mc/insert-numbers)
-;;;;;;  "mc-separate-operations" "mc-separate-operations.el" (20921
-;;;;;;  62328 916447 194000))
+;;;;;;  "mc-separate-operations" "mc-separate-operations.el" (20923
+;;;;;;  15466 753747 701000))
 ;;; Generated autoloads from mc-separate-operations.el
 
 (autoload 'mc/insert-numbers "mc-separate-operations" "\
@@ -215,7 +215,7 @@ Insert increasing numbers for each cursor, starting at 0 or ARG.
 ;;;***
 
 ;;;### (autoloads (set-rectangular-region-anchor) "rectangular-region-mode"
-;;;;;;  "rectangular-region-mode.el" (20921 62328 904447 193000))
+;;;;;;  "rectangular-region-mode.el" (20923 15466 685747 698000))
 ;;; Generated autoloads from rectangular-region-mode.el
 
 (autoload 'set-rectangular-region-anchor "rectangular-region-mode" "\
@@ -229,8 +229,8 @@ an exceedingly quick way of adding multiple cursors to multiple lines.
 ;;;***
 
 ;;;### (autoloads nil nil ("mc-cycle-cursors.el" "multiple-cursors-core.el"
-;;;;;;  "multiple-cursors-pkg.el" "multiple-cursors.el") (20921 62328
-;;;;;;  931258 549000))
+;;;;;;  "multiple-cursors-pkg.el" "multiple-cursors.el") (20923 15466
+;;;;;;  794299 176000))
 
 ;;;***
 
